f6f7b2c506ff130e6f1f4738a9d571d74523daab
Greyson <gmcreynolds3@gatech.edu>
2026-04-20 12:11:47 -0400

    Renamed to CleanedCrashData.csv, added environment.yml, added roadMapping.py with loadData and loadGraph methods

---
+++ roadMapping.py
@@ -0,0 +1,32 @@
+import os
+import osmnx as ox
+import pandas as pd
+from shapely.geometry import Point
+
+def loadData(path="data/CleanedCrashData.csv"):
+    if not os.path.exists(path):
+        raise FileNotFoundError("Crash csv not found")
+    try:
+        df = pd.read_csv(path)
+    except Exception as e:
+        raise ValueError(f"Failed to read csv. Error is {e}")
+    
+    if df.empty:
+        raise ValueError(f"Crash csv is empty")
+    
+    try:
+        df["geometry"] = [Point(xy) for xy in zip(df["lon"], df["lat"])]
+    except Exception as e:
+        raise ValueError("Failed to create Point geometry")
+
+    return df
+
+
+def loadGraph(path="atlantaGraph.graphml"):
+    if os.path.exists(path):
+        return ox.load_graphml(path)
+    else:
+        print("Making new graph")
+        graph = ox.graph_from_place("Atlanta, Georgia, USA", network_type='drive')
+        ox.save_graphml(graph, path)
+        return graph
