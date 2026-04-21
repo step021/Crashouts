d93951fc3999a9441d2431a734722e48830a9a67
Greyson <gmcreynolds3@gatech.edu>
2026-04-21 18:43:32 -0400

    Included Tables in penalty.py for calculate punishments

---
+++ penalty.py
@@ -0,0 +1,5 @@
+R_TABLE = {True: 1.55, False: 1}
+
+CT_TABLE = {'Rear End': .572, 'Sideswipe-Same Direction': .303, 'Angle': 1.23, 'Head On': 3.875, 'Sideswipe-Opposite Direction': .612, 'Not A Collision with Motor Vehicle': 10.645}
+
+CR_TABLE = {'K': 15, 'A': 5, 'B': 1, 'C': 1, 'O': .2}
