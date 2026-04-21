56d8e4260ab893c4e9abd6cf3e2c8db2aacdb97d
Greyson <gmcreynolds3@gatech.edu>
2026-04-21 18:25:11 -0400

    Replaced CleanedCrashData.csv with file that includes mannerOfCollision column

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ def main():
         df = roadMapping.addCrashesToEdges(df, graph)
         cachingLogic.saveMappedCrashes(df, "cache/mappedCrashes.pkl")
 
+    #Can delete this later, used to test if the pipeline is working.
     print(roadMapping.countCrashesPerEdge(df))
 
     print("Finished")
